99e4d2b7618cf8338bed70b88a3506199c21a23f
Linielt <danielhlee912@gmail.com>
2024-09-11 22:44:14 +0100

    Finished making dropdown

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+import "./styles.css";
 console.log("Hello!");
+
+const dropdownButton = document.getElementById("dropdown-button");
+const dropdownContent = document.getElementsByClassName("dropdown-content");
+
+dropdownButton.addEventListener("click", () => {
+  for (let content of dropdownContent) {
+    content.classList.toggle("show");
+  }
+});
